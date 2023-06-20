201ce1b326cc5a4e5efabf2129c32ccf454e8003
mabvmex <mabvmex@gmail.com>
2023-06-20 03:51:23 -0600

    feat: Pantalla inicial - maquetado y estilo completo

---
+++ index.js
@@ -0,0 +1,6 @@
+/*  DATE FOR FOOTER */
+
+const presentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = presentYear;
+// document.getElementById("year").textContent = presentYear;
+
