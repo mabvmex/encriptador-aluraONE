7fad9c2c982f47955bebd76178d95f29dfc4a101
mabvmex <mabvmex@gmail.com>
2023-06-25 10:26:07 -0600

    feat: JS -> Se crea función para tomar texto de panel izquierdo y mostrarlo en el panel derecho

--- index.js
+++ index.js
@@ -2,5 +2,42 @@
 
 const presentYear = new Date().getFullYear();
 document.querySelector("#year").textContent = presentYear;
-// document.getElementById("year").textContent = presentYear;
 
+/* FUNCIÓN PARA MOSTRAR TEXTO */
+
+function funcionMostrar() {
+    let textoEntrada = document.querySelector("#texto-entrada");
+    let textoSalida = document.querySelector("#cuadro-respuesta");
+
+    textoSalida.value = textoEntrada.value;
+
+    if (textoEntrada.value.length === 0) {
+        document.querySelector("#no-message-img-cluster").style.display =
+            "block";
+        return;
+    } else {
+        document.querySelector("#no-message-img-cluster").style.display =
+            "none";
+        textoEntrada.value = "";
+    }
+}
+/* 
+function convertirVocales() {
+    var input = document.getElementById("input1");
+    var valor = input.value;
+
+    valor = valor.replace(/[áÁ]/g, "a");
+    valor = valor.replace(/[éÉ]/g, "e");
+    valor = valor.replace(/[íÍ]/g, "i");
+    valor = valor.replace(/[óÓ]/g, "o");
+    valor = valor.replace(/[úÚ]/g, "u");
+
+    input.value = valor;
+} */
+
+/* 
+UTILS:
+========
+- document.getElementById("year").textContent = presentYear;
+- document.getElementById("encriptar").style.display = "none";
+*/
