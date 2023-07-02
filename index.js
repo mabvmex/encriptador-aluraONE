7aed016380cde111dc58a8a79c2412ff957ea1a5
mabvmex <mabvmex@gmail.com>
2023-07-02 16:25:11 -0600

    feat: JS:  Se crea la función y los mecanismos para lanzar notificaciónes

--- index.js
+++ index.js
@@ -1,47 +1,33 @@
 let textoEntrada = document.querySelector("#texto-entrada");
 let textoSalida = document.querySelector("#cuadro-respuesta");
 
-// function ejecutarEnter() {
-//     if (textoSalida.value !== 0) {
-//         mostrarTextoDescifrado();
-//     } else {
-//         mostrar();
-//     }
-// }
-
-/* FUNCIÓN PARA MOSTRAR TEXTO */
-// ==============================
-
-function mostrar() {
+/* FUNCIÓN PARA CIFRAR EL TEXTO */
+// ===============================
+function cifrarTexto() {
     let palabraCifrada = "";
-    /* FUNCIÓN PARA CIFRAR EL TEXTO */
-    // =============================
-    function cifrado() {
-        Array.from(textoEntrada.value).forEach((letra) => {
-            let letraCifrada = "";
+    Array.from(textoEntrada.value).forEach((letra) => {
+        let letraCifrada = "";
 
-            if (letra === "a") {
-                letraCifrada = "ai";
-            } else if (letra === "e") {
-                letraCifrada = "enter";
-            } else if (letra === "i") {
-                letraCifrada = "imes";
-            } else if (letra === "o") {
-                letraCifrada = "ober";
-            } else if (letra === "u") {
-                letraCifrada = "ufat";
-            } else {
-                letraCifrada = letra;
-            }
+        if (letra === "a") {
+            letraCifrada = "ai";
+        } else if (letra === "e") {
+            letraCifrada = "enter";
+        } else if (letra === "i") {
+            letraCifrada = "imes";
+        } else if (letra === "o") {
+            letraCifrada = "ober";
+        } else if (letra === "u") {
+            letraCifrada = "ufat";
+        } else {
+            letraCifrada = letra;
+        }
 
-            palabraCifrada += letraCifrada;
-        });
-    }
+        palabraCifrada += letraCifrada;
 
-    cifrado();
-
-    // textoSalida= palabraCifrada;
-    // if (textoSalida.value.length === 0) {
+        let tipoAlerta = ".alerta-cifrado";
+        document.querySelector(tipoAlerta).style.display = "flex";
+        clasesAnimacionAlerta(tipoAlerta);
+    });
 
     textoSalida.value = palabraCifrada;
     if (palabraCifrada.length === 0) {
@@ -52,8 +38,7 @@ function mostrar() {
 }
 
 /* FUNCIÓN PARA MOSTRAR TEXTO DESCIFRADO */
-// =============================
-
+// ========================================
 function mostrarTextoDescifrado() {
     let cifrado = textoEntrada.value;
     let palabraDescifrada = "";
@@ -68,12 +53,14 @@ function mostrarTextoDescifrado() {
     palabraDescifrada = cifrado.replace(
         /ai|enter|imes|ober|ufat/gi,
         (vocal) => {
+            let tipoAlerta = ".alerta-descifrado";
+            document.querySelector(tipoAlerta).style.display = "flex";
+            clasesAnimacionAlerta(tipoAlerta);
             return vocalCifrada[vocal];
         }
     );
 
     textoSalida.value = palabraDescifrada;
-
     if (palabraDescifrada.length === 0) {
         return limpiezaInputCasoUno();
     } else {
@@ -82,30 +69,34 @@ function mostrarTextoDescifrado() {
 }
 
 /* FUNCIÓN PARA COPIAR TEXTO */
-// ==============================
+// ============================
 function copiarResultado() {
     let textoCopiado = document.querySelector("#cuadro-respuesta");
+    let alerta = (document.querySelector(".alerta-copiado").style.display =
+        "flex");
 
     textoCopiado.focus();
     textoCopiado.setSelectionRange(0, -1);
     let texto = textoCopiado.value;
-
     // - document.execCommand("copy"); es sustituido por Clipboard API
     navigator.clipboard
-        .writeText(texto)
+        .writeText(texto, alerta)
         .then(() => {
             texto;
+            alerta;
         })
         .catch((err) => {
             console.log("Algo salió mal al copiar: " + err);
         });
     // texto.setSelectionRange(0, -1);
     textoEntrada.focus();
+
+    let tipoAlerta = ".alerta-copiado";
+    clasesAnimacionAlerta(tipoAlerta);
 }
 
-// FUNCIÓN REEMPLAZAR VOCALES CON ACENTO
-// ========================
-
+/* FUNCIÓN REEMPLAZAR VOCALES CON ACENTO */
+// ========================================
 function convertirVocales() {
     let typing = document.querySelector("#texto-entrada");
     let conversion = typing.value;
@@ -121,9 +112,8 @@ function convertirVocales() {
     typing.value = conversion;
 }
 
-// FUNCIONES DE OCULTAR/MOSTRAR EN INPUTS PRINCIAPLES
-// ========================
-
+/* FUNCIONES DE OCULTAR/MOSTRAR EN INPUTS PRINCIAPLES */
+// =====================================================
 function limpiezaInputCasoUno() {
     document.querySelector("#no-message-img-cluster").style.display = "block";
     document.querySelector("#copiar").style.display = "none";
@@ -138,7 +128,29 @@ function limpiezaInputCasoDos() {
     textoEntrada.focus();
 }
 
-/* FUNCIÓN  DATE PARA EL FOOTER */
+/*  FUNCIÓN PARA ANIMAR ALERTA DE NOTIFICACIÓN */
+// ==============================================
+function clasesAnimacionAlerta(tipoAlerta) {
+    const desapareceAlerta = setTimeout(limpiarAlerta, 1500);
+    function limpiarAlerta() {
+        document
+            .querySelector(tipoAlerta)
+            .classList.add("animate__bounceOutRight");
+    }
+    document
+        .querySelector(tipoAlerta)
+        .classList.remove("animate__bounceOutRight");
+}
+
+/* FUNCIÓN DATE PARA EL FOOTER */
 // ==============================
 const presentYear = new Date().getFullYear();
 document.querySelector("#year").textContent = presentYear;
+
+// function ejecutarEnter() {
+//     if (textoSalida.value !== 0) {
+//         mostrarTextoDescifrado();
+//     } else {
+//         mostrar();
+//     }
+// }
